a0b4e208b16e74973c68aae43fc9e07adc51ce2c
IT20600334 - Eranda Withanage <88669615+it20600334@users.noreply.github.com>
2024-05-19 02:21:19 +0530

    done

--- monkey_frontend/lib/Screens/CatchTheDamagerScreen.dart
+++ monkey_frontend/lib/Screens/CatchTheDamagerScreen.dart
@@ -1,43 +1,220 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:developer' as devtools;
 
-class CatchTheDamagerScreen extends StatelessWidget {
-  const CatchTheDamagerScreen({Key? key}) : super(key: key);
+class CatchTheDamagerScreen extends StatefulWidget {
+  @override
+  _CatchTheDamagerScreenState createState() => _CatchTheDamagerScreenState();
+}
+
+class _CatchTheDamagerScreenState extends State<CatchTheDamagerScreen> {
+  File? _image;
+  String label = '';
+  double confidence = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tfLteInit();
+  }
+
+  Future<void> _tfLteInit() async {
+    String? res = await Tflite.loadModel(
+        model: "assets/converted_model.tflite",
+        labels: "assets/labels.txt",
+        numThreads: 1, // defaults to 1
+        isAsset:
+            true, // defaults to true, set to false to load resources outside assets
+        useGpuDelegate:
+            false // defaults to false, set to true to use GPU delegate
+        );
+  }
+
+  Future<void> pickImageGallery() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image == null) return;
+
+    var imageFile = File(image.path);
+
+    setState(() {
+      _image = imageFile;
+    });
+
+    var recognitions = await Tflite.runModelOnImage(
+        path: image.path, // required
+        imageMean: 0.0, // defaults to 117.0
+        imageStd: 255.0, // defaults to 1.0
+        numResults: 2, // defaults to 5
+        threshold: 0.2, // defaults to 0.1
+        asynch: true // defaults to true
+        );
+
+    if (recognitions == null) {
+      devtools.log("recognitions is Null");
+      return;
+    }
+    devtools.log(recognitions.toString());
+    setState(() {
+      confidence = (recognitions[0]['confidence'] * 100);
+      label = recognitions[0]['label'].toString();
+    });
+  }
+
+  Future<void> pickImageCamera() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+    if (image == null) return;
+
+    var imageFile = File(image.path);
+
+    setState(() {
+      _image = imageFile;
+    });
+
+    var recognitions = await Tflite.runModelOnImage(
+        path: image.path, // required
+        imageMean: 0.0, // defaults to 117.0
+        imageStd: 255.0, // defaults to 1.0
+        numResults: 2, // defaults to 5
+        threshold: 0.2, // defaults to 0.1
+        asynch: true // defaults to true
+        );
+
+    if (recognitions == null) {
+      devtools.log("recognitions is Null");
+      return;
+    }
+    devtools.log(recognitions.toString());
+    setState(() {
+      confidence = (recognitions[0]['confidence'] * 100);
+      label = recognitions[0]['label'].toString();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 0, 255, 34),
+        backgroundColor: const Color.fromARGB(255, 0, 165, 22),
         title: const Text(
           'Catch the Damager',
           style: TextStyle(
-              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Catch the Damager Screen',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 0, 255, 34),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 12,
               ),
-            ),
-          ],
+              Card(
+                elevation: 20,
+                clipBehavior: Clip.hardEdge,
+                child: SizedBox(
+                  width: 300,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        Container(
+                          height: 280,
+                          width: 280,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            image: const DecorationImage(
+                              image: AssetImage('assets/upload.jpg'),
+                            ),
+                          ),
+                          child: _image == null
+                              ? const Text('')
+                              : Image.file(
+                                  _image!,
+                                  fit: BoxFit.fill,
+                                ),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Text(
+                                label,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                              // Text(
+                              //   "The Accuracy is ${confidence.toStringAsFixed(0)}%",
+                              //   style: const TextStyle(
+                              //     fontSize: 18,
+                              //   ),
+                              // ),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+                onPressed: pickImageCamera,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                  foregroundColor: Colors.black,
+                ),
+                child: const Text("Take a Photo"),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+                onPressed: pickImageGallery,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                  foregroundColor: Colors.black,
+                ),
+                child: const Text("Pick from gallery"),
+              ),
+            ],
+          ),
         ),
       ),
     );
